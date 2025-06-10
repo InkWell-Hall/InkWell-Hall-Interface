5caa49cbd97837b4c54935ea3bfeaee36929e610
Swatson-jnr <jkad.swatsonjnr@gmail.com>
2025-06-10 12:07:02 +0100

    fixed the error with the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import Dashboard from "./pages/Dashboard";
+
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import ResetPassword from "./pages/ResetPassword";
-import ForgotPassword from "./pages/ForgotPassword";
-import SignUp from "./pages/SignUp";
 
 const routes = createBrowserRouter([
   {
